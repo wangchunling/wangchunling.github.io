2e4cfd64f3db252dcf28d55d0f5b732eecf0f5e8
wcl_fxtrip <413349974@qq.com>
2015-09-28 16:40:23 +0800

    test

--- react-demo/app.jsx
+++ react-demo/app.jsx
@@ -55,7 +55,7 @@ var CouponListBodyItem = React.createClass({
                <h2 className="mt-04 mb-02 grey">订单编号：{this.props.oid} <span className="fr d-ib w-25 ta-c">订单状态</span></h2>
                <div className="list-item">
                    <div className="w-75 d-ib">
-                       <img className="img" src="3b6eb31064dbaa81a128541cb4ecf9fa.jpg" />
+                       <img className="img" src="demo1.jpg" />
                        <p>my demo </p>
                        <p>数量：<span className="orange">{this.props.count}</span>  金额：<span className="orange">￥{this.props.price}</span></p>
                        <p>有效期至 <span className="orange">{this.props.date}</span></p>
